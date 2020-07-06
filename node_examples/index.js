7287e1bda5b35b0e294abd848ce813aa6b652bc8
shorya361 <55752959+shorya361@users.noreply.github.com>
2020-07-06 16:44:58 +0530

    introduction to node modules

---
+++ node_examples/index.js
@@ -0,0 +1,18 @@
+var rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y,
+};
+
+function solveRect(l, b) {
+  console.log('----------------------------------------------------------');
+  console.log('solving for rectangle with l=' + l + ' and b=' + b);
+  if (l <= 0 || b <= 0) {
+    console.log('Rectangle dimensions should be greater than 0');
+  } else {
+    console.log('The area of rectangle is ' + rect.area(l, b));
+    console.log('The perimeter of rectangle is ' + rect.perimeter(l, b));
+  }
+}
+
+solveRect(5, 2);
+solveRect(-2, 5);
